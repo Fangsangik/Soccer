7a68d8dc5ac39f5756889f586e51e7fac039524c
hwang <hwangsangik@gmail.com>
2024-05-21 00:41:52 +0900

    feat : 축구

---
+++ src/main/java/football/footballplayer/FootballplayerApplication.java
@@ -0,0 +1,13 @@
+package football.footballplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/football/footballplayer/FootballplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package football.footballplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
